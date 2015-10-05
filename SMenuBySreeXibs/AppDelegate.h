7e24ade9d79682f9081e6f68fe26aff7366290a4
sreenivasulareddy <sreenivasulareddy@administrators-Mac-mini.local>
2015-10-05 12:22:13 +0530

    Initial Commit

---
+++ SMenuBySreeXibs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMenuBySreeXibs
+//
+//  Created by sreenivasulareddy on 05/10/15.
+//  Copyright (c) 2015 sreenivasulareddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SMenuBySreeXibs/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMenuBySreeXibs
+//
+//  Created by sreenivasulareddy on 05/10/15.
+//  Copyright (c) 2015 sreenivasulareddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
